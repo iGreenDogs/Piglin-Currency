af0f664fa3c05de2dda877d75a36296765f9813d
pub <example@example.com>
2022-03-10 17:17:34 -0500

    Continued to make piglins love items. Now all piglins don't exist

--- src/main/java/net/piglin/mixin/PiglinGold.java
+++ src/main/java/net/piglin/mixin/PiglinGold.java
@@ -10,12 +10,12 @@ import net.minecraft.entity.mob.PiglinBrain;
 import net.piglin.*;
 
 @Mixin(PiglinBrain.class)
-public abstract class PiglinTrades {
-    @Inject(method = "acceptsForBarter", at = @At("HEAD"), cancellable = true)
+public abstract class PiglinGold {
+    @Inject(method = "isGoldenItem", at = @At("HEAD"), cancellable = true)
     private static void injected(ItemStack stack, CallbackInfoReturnable ci) {
         if((stack.getItem() == PiglinCurrency.PIG_COIN)||(stack.getItem() == PiglinCurrency.GOLD_COIN)||(stack.getItem() == PiglinCurrency.NETHERITE_COIN)||(stack.getItem() == PiglinCurrency.COMPRESSED_COIN)) {
             ci.setReturnValue(true);
         }
     }
-    
+
 }
